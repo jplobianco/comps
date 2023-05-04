619dd343e2d67c22186da067690e5795243ab072
João Paulo Lobianco Silva <joao.lobianco@al.go.leg.br>
2023-05-04 08:20:52 -0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Button primary>Click me</Button>
+      </div>
+      <div>
+        <Button>Buy me</Button>
+      </div>
+      <div>
+        <Button>abc</Button>
+      </div>
+      <div>
+        <Button>def</Button>
+      </div>
+      <div>
+        <Button>ghi</Button>
+      </div>
     </div>
   );
 }
