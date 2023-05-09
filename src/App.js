6cff885a89a9722b2ca6d5d471a988f50766c771
João Paulo Lobianco <joaopaulolobianco1@gmail.com>
2023-05-08 22:14:41 -0300

    wip: accordion component

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import Accordion from "./components/Accordion";
 function App() {
   const items = [
     {
-      id: "dkfjaslf",
+      id: "abc",
       label: "Can I use React on a project?",
       content:
         "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
     },
     {
-      id: "fafdas",
+      id: "def",
       label: "Can I use Javascript on a project?",
       content:
         "You can use Javascript on any project you want. You can use Javascript on any project you want. You can use Javascript on any project you want.",
     },
     {
-      id: "fdsafas",
+      id: "ghi",
       label: "Can I use Python on a project?",
       content:
         "You can use Python on any project you want. You can use Python on any project you want. You can use Python on any project you want.",
