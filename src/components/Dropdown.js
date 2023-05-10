512a0ae785243a4ec00b4a6956c5f9e81ff6f770
João Paulo Lobianco Silva <joao.lobianco@al.go.leg.br>
2023-05-10 08:08:22 -0300

    wip

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,17 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { GoChevronDown } from "react-icons/go";
 import Panel from "./Panel";
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
+  const divEl = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (!divEl.current) {
+        return;
+      }
+
+      if (!divEl.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("click", handler, true);
+
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
 
   const handleClick = (current) => {
     setIsOpen(!current);
   };
 
-  window.timeTwo = performance.now();
   const handleOptionClick = (option) => {
-    window.timeOne = performance.now();
     setIsOpen(false);
     onChange(option);
   };
@@ -29,7 +45,7 @@ function Dropdown({ options, value, onChange }) {
   });
 
   return (
-    <div className="w-48 relative">
+    <div ref={divEl} className="w-48 relative">
       <Panel
         className="flex justify-between items-center cursor-pointer"
         onClick={() => handleClick(isOpen)}
