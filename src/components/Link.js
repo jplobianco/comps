dee55224d46ad4271e3758077d1a9d35141f4bfe
João Paulo Lobianco <joaopaulolobianco1@gmail.com>
2023-05-12 16:07:20 -0300

    fix(Link): add missing key prop to Link component

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,9 +1,13 @@
 import classNames from "classnames";
 import useNavigation from "../hooks/use-navigation";
 
-function Link({ to, children }) {
-  const { navigate } = useNavigation();
-  const classes = classNames("text-blue-500");
+function Link({ to, key, children, className, activeClassName }) {
+  const { navigate, currentPath } = useNavigation();
+  const classes = classNames(
+    "text-blue-500",
+    className,
+    currentPath === to && activeClassName
+  );
   const handleClick = (event) => {
     if (event.ctrlKey || event.metaKey) {
       return;
